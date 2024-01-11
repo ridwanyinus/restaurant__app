398e1052605d5dbe9d38bb1fef9cb55e1518f47e
officialyaro9 <ridwanyinusa15@gmail.com>
2024-01-11 15:36:26 +0100

    ###

--- src/app/sections/Dining.jsx
+++ src/app/sections/Dining.jsx
@@ -1,8 +1,14 @@
-
 const Dining = () => {
   return (
-    <section className="">Dining</section>
-  )
-}
+    <section className="pt-12 lg:pt-24">
+      <h2 className="text-yellow text-3xl xs:text-4xl sm:text-[40px]  md:text-5xl xl:text-6xl 2xl:text-[65px] font-display leading-normal mb-8 xs:mb-12 sm:mb-16 lg:mb-12 text-center">
+        Dining Events
+      </h2>
+      <p className="max-sm:px-6 text-white font-sans xs:text-lg sm:text-2xl md:text-3xl xl:text-[40px]  2xl:text-[45px] text-center">
+        We provide dining event for your special day <span className="sm:block mt-2 md:mt-4"> with your important people</span>
+      </p>
+    </section>
+  );
+};
 
-export default Dining
+export default Dining;
