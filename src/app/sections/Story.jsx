07cc50b754b9831f5c95dac5adfd59518ef483ad
officialyaro9 <ridwanyinusa15@gmail.com>
2024-01-12 17:17:46 +0100

    y

--- src/app/sections/Story.jsx
+++ src/app/sections/Story.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRef } from "react";
-import { useIsVisible } from "@app/Components/useIsVisible";
+import useIsVisible from "@app/Components/useIsVisible";
+
+
 
 import Image from "next/image";
 import story from "public/assets/story.png";
